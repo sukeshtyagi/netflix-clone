a98a2df06e9bd29e5f7014bf0741c93583962de4
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-08 16:28:16 +0530

    ChoosePlan component and all its components css completed

--- src/components/ChoosePlan.jsx
+++ src/components/ChoosePlan.jsx
@@ -17,19 +17,19 @@ function ChoosePlan() {
         <p className={style.contentDivLine1}>Step 2 of 3</p>
         <h1 className={style.contentDivHeading1}>Choose your plan.</h1>
 
-        <div className={style.lineDiv1}>
+        <div className={style.lineDivContainer}>
           <FontAwesomeIcon icon={faCheck} className={style.icon} />
           <p className={style.lineDivLine1}>No commitments, cancel anytime.</p>
         </div>
 
-        <div className={style.lineDiv2}>
-          <FontAwesomeIcon icon={faCheck} className={style.icon} />
+        <div className={style.lineDivContainer}>
+          <FontAwesomeIcon icon={faCheck} className={style.icon2} />
           <p className={style.lineDivLine2}>
             Everything on Netflix for one low price.
           </p>
         </div>
 
-        <div className={style.lineDiv3}>
+        <div className={style.lineDivContainer}>
           <FontAwesomeIcon icon={faCheck} className={style.icon} />
           <p className={style.lineDivLine3}>No ads and no extra fees. Ever.</p>
         </div>
