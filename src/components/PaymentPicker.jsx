dfc6be32be22597ba40bb997225045de6b5ebaa9
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-09 18:27:22 +0530

    Parent div (contentDiv) css done

--- src/components/PaymentPicker.jsx
+++ src/components/PaymentPicker.jsx
@@ -6,27 +6,29 @@ import { faArrowRight, faLock } from "@fortawesome/free-solid-svg-icons";
 function PaymentPicker() {
   return (
     <div className={style.paymentContainer}>
-      <p className="step">step 3 of 3</p>
-      <h1 className="heading">Choose how to pay</h1>
-      <p className="description">
-        Your payment is encrypted and you can change your payment method any
-        time.
-      </p>
-      <p className="line4">Secure for peace of mind.</p>
-      <p className="line">Cancel easy online.</p>
-      <div className="encryption">
-        <p className="text">End-to-end encrypted</p>
-        <FontAwesomeIcon icon={faLock} />
-      </div>
+      <div className={style.contentDiv}>
+        <p className={style.step}>Step 3 of 3</p>
+        <h1 className={style.heading}>Choose how to pay</h1>
+        <p className={style.description}>
+          Your payment is encrypted and you can change your payment method any
+          time.
+        </p>
+        <p className={style.line4}>Secure for peace of mind.</p>
+        <p className={style.line5}>Cancel easy online.</p>
+        <div className={style.encryption}>
+          <p className={style.text}>End-to-end encrypted</p>
+          <FontAwesomeIcon icon={faLock} className={style.icon} />
+        </div>
 
-      <div className="card">
-        <p className="text">Credit or Debit Card</p>
-        <FontAwesomeIcon icon={faArrowRight} />
-      </div>
+        <div className={style.card}>
+          <p className={style.text}>Credit or Debit Card</p>
+          <FontAwesomeIcon icon={faArrowRight} className={style.icon} />
+        </div>
 
-      <div className="upi">
-        <p className="text">UPI Auotpay</p>
-        <FontAwesomeIcon icon={faArrowRight} />
+        <div className={style.upi}>
+          <p className={style.text}>UPI Autopay</p>
+          <FontAwesomeIcon icon={faArrowRight} className={style.icon} />
+        </div>
       </div>
     </div>
   );
