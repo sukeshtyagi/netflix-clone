30674778029055b84aed952ab9908d678759d093
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-09 11:42:41 +0530

    PlanForm css completed

--- src/components/PlanForm.jsx
+++ src/components/PlanForm.jsx
@@ -5,160 +5,157 @@ function PlanForm() {
   return (
     <div className={style.formContainer}>
       <div className={style.contentDiv}>
-        <p className="step">Step 2 of 3</p>
-        <h1 className="heading">Choose plan that's right for you</h1>
-        <p className="popular">Popular</p>
-        <div className="planContainer">
-          <div className="planDetails">
-            <div className="planName">
-              <p className="normal">Premium</p>
-              <p className="quality">4k+HDR</p>
+        <p className={style.step}>Step 2 of 3</p>
+        <h1 className={style.heading}>Choose plan that's right for you</h1>
+        <p className={style.popular}>Most popular</p>
+        <div className={style.planContainer}>
+          <div className={style.planDetails}>
+            <div className={style.planNamePremium}>
+              <p className={style.normal}>Premium</p>
+              <p className={style.quality}>4k+HDR</p>
             </div>
-            <div className="detail">
-              <p className="line1">MonthlyPrice</p>
-              <p className="line2">Rs</p>
-              <div className="detail">
-                <p className="line1">Videoandsoundquality</p>
-                <p className="line2">entersoundQuality</p>
-                <div className="detail">
-                  <p className="line1">Resolution</p>
-                  <p className="line2">ResoulutinQuality</p>
-                  <div className="detail">
-                    <p className="line1">SpatialAudioImmersiveSound</p>
-                    <p className="line2">SpatialAudioImmersiveSound</p>
-                    <div className="detail">
-                      <p className="line1">SupportedDevices</p>
-                      <p className="line2">SupportedDevices</p>
-                      <div className="detail">
-                        <p className="line1">
-                          Devicesyourhouseholdcanwatchatthesametiem
-                        </p>
-                        <p className="line2">
-                          Devicesyourhouseholdcanwatchatthesametiem
-                        </p>
-                        <div className="detail">
-                          <p className="line1">Downloaddevices</p>
-                          <p className="line2">Downloaddevices</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Monthly Price</p>
+              <p className={style.line2}>Rs 649</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Video and sound quality</p>
+              <p className={style.line2}>Best</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Resolution</p>
+              <p className={style.line2}>4K (Ultra HD) + HDR</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Spatial Audio Immersive Sound</p>
+              <p className={style.line2}>Included</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Supported Devices</p>
+              <p className={style.line2}>TV, computer, mobile phone, tablet</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>
+                Devices your household can watch at the same time
+              </p>
+              <p className={style.line2}>4</p>
+            </div>
+            <div className={style.downloadDetail}>
+              <p className={style.line1}>Download devices</p>
+              <p className={style.line2}>6</p>
             </div>
           </div>
-          <div className="planDetails">
-            <div className="planName">
-              <p className="normal">Premium</p>
-              <p className="quality">4k+HDR</p>
+
+          <div className={style.planDetails}>
+            <div className={style.planNameStandard}>
+              <p className={style.normal}>Standard</p>
+              <p className={style.quality}>1080p</p>
             </div>
-            <div className="detail">
-              <p className="line1">MonthlyPrice</p>
-              <p className="line2">Rs</p>
-              <div className="detail">
-                <p className="line1">Videoandsoundquality</p>
-                <p className="line2">entersoundQuality</p>
-                <div className="detail">
-                  <p className="line1">Resolution</p>
-                  <p className="line2">ResoulutinQuality</p>
-                  <div className="detail">
-                    <p className="line1">SpatialAudioImmersiveSound</p>
-                    <p className="line2">SpatialAudioImmersiveSound</p>
-                    <div className="detail">
-                      <p className="line1">SupportedDevices</p>
-                      <p className="line2">SupportedDevices</p>
-                      <div className="detail">
-                        <p className="line1">
-                          Devicesyourhouseholdcanwatchatthesametiem
-                        </p>
-                        <p className="line2">
-                          Devicesyourhouseholdcanwatchatthesametiem
-                        </p>
-                        <div className="detail">
-                          <p className="line1">Downloaddevices</p>
-                          <p className="line2">Downloaddevices</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Monthly Price</p>
+              <p className={style.line2}>Rs 499</p>
             </div>
-          </div>
-          <div className="planDetails">
-            <div className="planName">
-              <p className="normal">Premium</p>
-              <p className="quality">4k+HDR</p>
+            <div className={style.detail}>
+              <p className={style.line1}>Video and sound quality</p>
+              <p className={style.line2}>Great</p>
             </div>
-            <div className="detail">
-              <p className="line1">MonthlyPrice</p>
-              <p className="line2">Rs</p>
-              <div className="detail">
-                <p className="line1">Videoandsoundquality</p>
-                <p className="line2">entersoundQuality</p>
-                <div className="detail">
-                  <p className="line1">Resolution</p>
-                  <p className="line2">ResoulutinQuality</p>
-                  <div className="detail">
-                    <p className="line1">SpatialAudioImmersiveSound</p>
-                    <p className="line2">SpatialAudioImmersiveSound</p>
-                    <div className="detail">
-                      <p className="line1">SupportedDevices</p>
-                      <p className="line2">SupportedDevices</p>
-                      <div className="detail">
-                        <p className="line1">
-                          Devicesyourhouseholdcanwatchatthesametiem
-                        </p>
-                        <p className="line2">
-                          Devicesyourhouseholdcanwatchatthesametiem
-                        </p>
-                        <div className="detail">
-                          <p className="line1">Downloaddevices</p>
-                          <p className="line2">Downloaddevices</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Resolution</p>
+              <p className={style.line2}>1080p(Full HD)</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Spatial Audio Immersive Sound</p>
+              <p className={style.line2}>Excluded</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Supported Devices</p>
+              <p className={style.line2}>TV, computer, mobile phone, tablet</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>
+                Devices your household can watch at the same time
+              </p>
+              <p className={style.line2}>2</p>
+            </div>
+            <div className={style.downloadDetail}>
+              <p className={style.line1}>Download devices</p>
+              <p className={style.line2}>2</p>
             </div>
           </div>
-          <div className="planDetails">
-            <div className="planName">
-              <p className="normal">Premium</p>
-              <p className="quality">4k+HDR</p>
+
+          <div className={style.planNameBasic}>
+            <div className={style.planName}>
+              <p className={style.normal}>Basic</p>
+              <p className={style.quality}>720p</p>
             </div>
-            <div className="detail">
-              <p className="line1">MonthlyPrice</p>
-              <p className="line2">Rs</p>
-              <div className="detail">
-                <p className="line1">Videoandsoundquality</p>
-                <p className="line2">entersoundQuality</p>
-                <div className="detail">
-                  <p className="line1">Resolution</p>
-                  <p className="line2">ResoulutinQuality</p>
-                  <div className="detail">
-                    <p className="line1">SpatialAudioImmersiveSound</p>
-                    <p className="line2">SpatialAudioImmersiveSound</p>
-                    <div className="detail">
-                      <p className="line1">SupportedDevices</p>
-                      <p className="line2">SupportedDevices</p>
-                      <div className="detail">
-                        <p className="line1">
-                          Devicesyourhouseholdcanwatchatthesametiem
-                        </p>
-                        <p className="line2">
-                          Devicesyourhouseholdcanwatchatthesametiem
-                        </p>
-                        <div className="detail">
-                          <p className="line1">Downloaddevices</p>
-                          <p className="line2">Downloaddevices</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Monthly Price</p>
+              <p className={style.line2}>Rs 199</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Video and sound quality</p>
+              <p className={style.line2}>Good</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Resolution</p>
+              <p className={style.line2}>720p(HD)</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Spatial Audio Immersive Sound</p>
+              <p className={style.line2}>Excluded</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Supported Devices</p>
+              <p className={style.line2}>
+                TV, computer, mobile phone, tablet,{" "}
+              </p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>
+                Devices your household can watch at the same time
+              </p>
+              <p className={style.line2}>1</p>
+            </div>
+            <div className={style.downloadDetail}>
+              <p className={style.line1}>Download devices</p>
+              <p className={style.line2}>1</p>
+            </div>
+          </div>
+
+          <div className={style.planDetails}>
+            <div className={style.planName}>
+              <p className={style.normal}>Mobile</p>
+              <p className={style.quality}>480p</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Monthly Price</p>
+              <p className={style.line2}>Rs 149</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Video and sound quality</p>
+              <p className={style.line2}>Fair</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Resolution</p>
+              <p className={style.line2}>480p</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Spatial Audio Immersive Sound</p>
+              <p className={style.line2}>Excluded</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>Supported Devices</p>
+              <p className={style.line2}>Mobile phone , tablet</p>
+            </div>
+            <div className={style.detail}>
+              <p className={style.line1}>
+                Devices your household can watch at the same time
+              </p>
+              <p className={style.line2}>1</p>
+            </div>
+            <div className={style.downloadDetail}>
+              <p className={style.line1}>Download devices</p>
+              <p className={style.line2}>1</p>
             </div>
           </div>
         </div>
