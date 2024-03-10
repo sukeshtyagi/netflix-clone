97fe05264bc31189cdb6ef4d40593eec6e8b9cf9
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-10 13:44:38 +0530

    styling of CreditOption comp completed

--- src/components/CreditOption.jsx
+++ src/components/CreditOption.jsx
@@ -33,47 +33,52 @@ function CreditOption() {
           </div>
 
           <div className={style.cardDetails}>
-            <input type="text" className={style.expiryDate} />
+            <input
+              type="text"
+              className={style.expiryDate}
+              placeholder="Expiration date"
+            />
 
             <div className={style.cvvDetails}>
-              <input type="text" className={style.cvvDate} />
+              <input type="text" className={style.cvvDate} placeholder="CVV" />
               <FontAwesomeIcon icon={faLock} className={style.lockIcon} />
             </div>
           </div>
 
-          <div className={style.cardHolderName}>
-            <input
-              type="text"
-              className={style.name}
-              placeholder="Name on card"
-            />
-          </div>
-        </div>
+          <input
+            type="text"
+            className={style.name}
+            placeholder="Name on card"
+          />
 
-        <div className={style.planDetail}>
-          <p className={style.cost}>&#x20B9; {price}/month</p>
-          <p className={style.planName}>{plan}</p>
-          <NavLink className={style.navlinkChangePlan}>Change</NavLink>
+          <div className={style.planDetail}>
+            <div className={style.planInnerDiv}>
+              <p className={style.cost}>&#x20B9; {price}/month</p>
+              <p className={style.planName}>{plan}</p>
+            </div>
+            <NavLink className={style.navlinkChangePlan}>Change</NavLink>
+          </div>
+          <p className={style.info1}>
+            Any payment above &#x20B9; 2000 shall need additional
+            authentification.
+          </p>
+          <p className={style.info2}>
+            By checking the checkBox below, you agree to our
+            <NavLink className={style.navlinkInfo}>
+              Terms of Use, Privacy Statement
+            </NavLink>
+            , and that you're over 18.Netflix will automatically continue your
+            membership and charge the membership fee(currently &#x20B9; {price}
+            /month) to your payment method untill you cancel.You may cancel at
+            any time to avoid future charges.
+          </p>
+          <button className={style.contentDivButton}>Start Membership</button>
+          <p className={style.captcha}>
+            This page is protected by google reCAPTCHA to ensure you're not a
+            bot.
+            <NavLink className={style.navlinkCaptcha}>Learn more</NavLink>
+          </p>
         </div>
-        <p className={style.info1}>
-          Any payment above &#x20B9; 2000 shall need additional
-          authentification.
-        </p>
-        <p className={style.info2}>
-          By checking the checkBox below, you agree to our
-          <NavLink className={style.navlinkInfo}>
-            Terms of Use, Privacy Statement
-          </NavLink>
-          , and that you're over 18.Netflix will automatically continue your
-          membership and charge the membership fee(currently &#x20B9; {price}
-          /month) to your payment method untill you cancel.You may cancel at any
-          time to avoid future charges.
-        </p>
-        <button className={style.contentDivButton}>Start Membership</button>
-        <p className={style.captcha}>
-          This page is protected by google reCAPTCHA to ensure you're not a bot.
-          <NavLink className={style.navlinkCaptcha}>Learn more</NavLink>
-        </p>
       </div>
     </div>
   );
